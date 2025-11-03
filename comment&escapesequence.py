8b8ea05826dcea1c61d06ddfc0ff7920dc6bf8f5
Saurabhchougale <saurabhchougale45@gmail.com>
2025-11-03 08:37:42 +0530

    Add files via upload

---
+++ comment&escapesequence.py
@@ -0,0 +1,12 @@
+#This is a single-line comment
+print("This is a print statement.")
+print("Hello World!") #printing Hello World 
+
+'''
+This is multiple-line comment
+Hey I am youtuber
+The next line describes someone's quality and their ability to learn new thing
+'''
+print("Hey I am a \"good boy\" and \nI am learn new language ")
+print("hey",4,8,sep="=",end="087\n")
+print("THis is python")
