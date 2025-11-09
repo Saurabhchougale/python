244cd4ff3cb67ebeba40b8d04d701cd4f65f3b23
Saurabhchougale <saurabhchougale45@gmail.com>
2025-11-09 15:55:26 +0530

    Add files via upload

---
+++ variableanddatatype.py
@@ -0,0 +1,15 @@
+a=161
+print(a)
+b="Saurabh"
+print(b)
+c=True
+print(c)
+d=None
+print(d)
+e=24.3
+print(a+e)
+print("Data type of A is :",type(a))
+print("Data type of B is :",type(b))
+print("Data type of C is :",type(c))
+print("Data type of D is :",type(d))
+print("Data type of E is :",type(e))
